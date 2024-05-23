9c6027df9fd92e6d8d9786ae4de99fbaa0c98de6
kf7091 <kf7091@mci4me.at>
2024-05-24 01:09:33 +0200

    add plotly figures with colorcoding

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-import pandas as pd
-import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 from my_functions import read_activity_data
 
 
 activity_data = read_activity_data()
 
-zone_colors = {
-    "keine Angabe": "weiß",
-    "sehr leicht": "grün",
-    "leicht": "gelb",
-    "moderat": "orange",
-    "hart": "rot",
-    "sehr hart": "schwarz"
-}
+fig_hr = go.Figure(
+    data=go.Scatter(
+        x=activity_data["Duration"],
+        y=activity_data["HeartRate"],
+        marker = {"color" : activity_data["Color"]},
+        line = {"color" : "lightgray"},
+        mode='lines+markers'
+    )
+)
+fig_hr.show()
+
+fig_power = go.Figure(
+    data=go.Scatter(
+        x=activity_data["Duration"],
+        y=activity_data["PowerOriginal"],
+        marker = {"color" : activity_data["Color"]},
+        line = {"color" : "lightgray"},
+        mode='lines+markers'
+    )
+)
+fig_power.show()
 
