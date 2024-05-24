6dc27baa03a8368f3354a85ef1dc3cf55d1b8c8c
Ihr Name auf github <hl5162@mci4me.at>
2024-05-24 11:02:25 +0200

    Plotly auf Streamlit (FUNKTIONIERT)

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import plotly.express as px
 import plotly.graph_objects as go
 from my_functions import read_activity_data
+import streamlit as st
+
+# Session State wird leer angelegt, solange er noch nicht existiert
+if 'current_user' not in st.session_state:
+    st.session_state.current_user = 'None'
+
 
 
 activity_data = read_activity_data()
@@ -11,10 +17,17 @@ fig_hr = go.Figure(
         y=activity_data["HeartRate"],
         marker = {"color" : activity_data["Color"]},
         line = {"color" : "lightgray"},
-        mode='lines+markers'
+        mode='lines+markers',
+        name = 'HeartRate'
     )
 )
-fig_hr.show()
+fig_hr.update_layout(title='Heartrate Zones',
+                   xaxis_title='Time [s]',
+                   yaxis_title='Heart Rate [bpm]',
+                   showlegend=False,
+                   legend_title="Zone colours:",)
+
+# fig_hr.show()
 
 fig_power = go.Figure(
     data=go.Scatter(
@@ -22,8 +35,21 @@ fig_power = go.Figure(
         y=activity_data["PowerOriginal"],
         marker = {"color" : activity_data["Color"]},
         line = {"color" : "lightgray"},
-        mode='lines+markers'
+        mode='lines+markers',
+        name = 'Power',
     )
 )
-fig_power.show()
+
+fig_power.update_layout(title='Power and Heartrate Zones',
+                   xaxis_title='Time [s]',
+                   yaxis_title='Power [W]',
+                   showlegend=False,
+                   legend_title="Zone colours:",                                
+                   )
+
+# fig_power.show()
+
+st.plotly_chart(fig_hr)
+st.plotly_chart(fig_power)
+
 
